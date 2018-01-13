204cb4e8c8a4630318f539af1217d35c593f9869
Mihkel Eidast <mihkel@play.ee>
2018-01-13 21:21:46 +0200

    [GRZ-154] remove test components

--- src/components/editor/editor.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default class Editor extends React.Component<{}, {}> {
-    render() {
-        return (
-            <div className="editor">hello world 7</div>
-        );
-    }
-}
--- src/components/test/test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-export default class Test extends React.Component<{}, {}> {
-    render() {
-        return (
-            <div className="test">hello world 2</div>
-        );
-    }
-}
