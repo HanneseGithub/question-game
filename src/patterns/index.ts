522b41847de0666470a262b5d3f4dd1a67ba155c
Mihkel Eidast <mihkel@play.ee>
2018-11-03 13:12:29 +0200

    [GRZ-154] Select: add component

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -22,6 +22,8 @@ export { default as Icon } from '@icon';
 
 export { default as Radio } from '@radio';
 
+export { default as Select } from '@select';
+
 export { default as TextArea} from '@textarea';
 export { default as TextField } from '@textfield';
 export { default as Typography } from '@typography';
