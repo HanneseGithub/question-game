2ea792b2ff0865f0b041b7053c6d4bc43ee79eb0
Mihkel Eidast <mihkel@play.ee>
2018-11-03 14:57:32 +0200

    [GRZ-154] remove header from Login view

--- src/patterns/views/base/base.tsx
+++ src/patterns/views/base/base.tsx
@@ -4,14 +4,24 @@ import Header, { IHeaderProps } from '@m-header';
 import Main from '@main';
 
 export interface IViewBaseProps {
-    header: IHeaderProps;
+    header?: IHeaderProps;
 }
 
 export default class ViewBase extends React.Component<IViewBaseProps> {
+    renderHeader(): JSX.Element | null {
+        if (!this.props.header) {
+            return null;
+        }
+
+        return (
+            <Header {...this.props.header} />
+        );
+    }
+
     render(): JSX.Element {
         return (
             <React.Fragment>
-                <Header {...this.props.header} />
+                {this.props.header && this.renderHeader()}
                 <Main>
                     {this.props.children}
                 </Main>
