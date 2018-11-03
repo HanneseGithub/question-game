dd6681269d24ec8f84c634047d7271053bb8ae30
Mihkel Eidast <mihkel@play.ee>
2018-11-03 14:52:21 +0200

    [GRZ-154] add Login view

--- src/patterns/index.fractal.ts
+++ src/patterns/index.fractal.ts
@@ -11,3 +11,4 @@ export { default as TypographyExample } from '@typography--example';
  * View examples
  */
 export { default as ViewBase } from '@v-base';
+export { default as ViewLogin } from '@v-login';
