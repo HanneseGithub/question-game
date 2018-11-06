640cc9f282ad3d8364a32ccde9ceeb172de34e06
Mihkel Eidast <mihkel@play.ee>
2018-11-06 19:36:02 +0200

    [GRZ-154] add Languages component to Header module

--- src/patterns/modules/header/header.tsx
+++ src/patterns/modules/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Container from '@container';
+import Languages, { ILanguagesProps } from '@languages';
 import Navigation, { INavigationProps } from '@navigation';
 
 if (process.env.webpack) {
@@ -9,6 +10,7 @@ if (process.env.webpack) {
 
 export interface IHeaderProps {
     navigation: INavigationProps;
+    languages: ILanguagesProps;
 }
 
 export default class Header extends React.Component<IHeaderProps> {
@@ -18,6 +20,7 @@ export default class Header extends React.Component<IHeaderProps> {
                 <Container>
                     <div className="header__inner">
                         <Navigation {...this.props.navigation} />
+                        <Languages {...this.props.languages} />
                     </div>
                 </Container>
             </header>
