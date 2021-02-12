598fbd691ccb73850330fa1b7aede1e5d1b46ea7
Andres Kalle <andres@play.ee>
2021-02-12 09:06:37 +0000

    Merged in bugfix/GRZ-519 (pull request #10)
    [GRZ-519]: extend global Window interface without redeclaring 'window'
    
    * [GRZ-519]: extend global Window interface without redeclaring 'window'
    
    Approved-by: Mihkel Eidast

--- types/window/index.d.ts
+++ types/window/index.d.ts
@@ -1,19 +1,22 @@
-interface Window {
-    components: {
-        [key: string]: string;
-    };
-    componentSettings: IComponentSettings;
-    ssr: boolean;
+declare global {
+    interface Window {
+        components: {
+            [key: string]: string;
+        };
+        componentSettings: IComponentSettings;
+        ssr: boolean;
+    }
+
+    interface IContext {
+        [key: string]: React.ReactNode;
+    }
+
+    interface IComponentSettings {
+        className: string;
+        context: IContext;
+        parseJsxFrom?: string[];
+    }
 }
 
-interface IContext {
-    [key: string]: React.ReactNode;
-}
-
-interface IComponentSettings {
-    className: string;
-    context: IContext;
-    parseJsxFrom?: string[];
-}
-
-declare let window: Window;
+// There must be at least one import or export for the file to be treated as a module
+export {};
