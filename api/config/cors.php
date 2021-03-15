427c492eaa3b9e3aeefdffb4219914a298c3bd44
Hannes Juurma <hannes@play.ee>
2021-03-15 12:28:17 +0200

    Question game api made with Laravel framework.

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://localhost:9000'],
 
     'allowed_origins_patterns' => [],
 
